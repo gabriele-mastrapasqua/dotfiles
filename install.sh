a541e9ac4ed9d2cb3ef0e193c68aa8dcb3eed73b
gabrielem0 <gabriele.mastrapasqua@gmail.com>
2017-10-18 22:51:40 +0200

    added git config

--- install.sh
+++ install.sh
@@ -4,6 +4,10 @@ echo "installing dev dependecies..."
 apt-get update && apt-get upgrade
 apt-get -y install git python3-dev python3-pip silversearcher-ag vim emacs build-essentials htop glances 
 
+# set git local account
+git config --global user.email  "gabriele.mastrapasqua@gmail.com"
+git config --global user.name  "gabrielem0"
+
 echo "installing emacs config..."
 cp -R emacs ~/.emacs.d
 

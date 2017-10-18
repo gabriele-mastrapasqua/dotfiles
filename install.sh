0b51d7b3ccd7898d20c5a85e448b804513afcc06
gabrielem0 <gabriele.mastrapasqua@gmail.com>
2017-10-18 22:49:48 +0200

    added minimal vimrc

--- install.sh
+++ install.sh
@@ -7,3 +7,6 @@ apt-get -y install git python3-dev python3-pip silversearcher-ag vim emacs build
 echo "installing emacs config..."
 cp -R emacs ~/.emacs.d
 
+echo "installing minimal vimrc..."
+cp .vimrc ~/.vimrc
+

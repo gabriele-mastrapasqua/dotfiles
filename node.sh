528476e1118566f8e12bfe385dc7f176eb4d5520
gabrielem0 <gabriele.mastrapasqua@gmail.com>
2017-10-19 19:48:00 +0200

    added node script

---
+++ node.sh
@@ -0,0 +1,8 @@
+# /bin/bash
+
+# follow nodesource setup https://github.com/nodesource/distributions
+
+# Using Ubuntu
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
